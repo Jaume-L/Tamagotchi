48f8e63b525a97112d187e91e41246223e4eea15
Louis Jaume <jaume.l@etoncollege.org.uk>
2023-01-14 10:58:55 +0000

    Initial Commit

---
+++ Tamagotchi/TamagotchiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TamagotchiApp.swift
+//  Tamagotchi
+//
+//  Created by Jaume, Louis (NA) on 14/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct TamagotchiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
